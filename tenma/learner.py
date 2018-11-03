bbc4a372fb7212160a51891acfb66969380f016d
KTaskn <korai.kinjo@gmail.com>
2018-11-03 14:45:56 +0900

    しゅうせい

--- tenma/learner.py
+++ tenma/learner.py
@@ -1,8 +1,5 @@
 # coding:utf-8
-import numpy as np
-import pandas as pd
 from tenma import dataload, abilitymodel, comparemodel
-from tenma.rulemodel import Rule
 
 if __name__ == "__main__":
     df = dataload.load()
