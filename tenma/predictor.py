469d8d25ac9207b51ba6a15df9c243f7f06fc7ab
KTaskn <korai.kinjo@gmail.com>
2018-11-03 14:43:40 +0900

    ブログ用に出力する

--- tenma/predictor.py
+++ tenma/predictor.py
@@ -6,25 +6,54 @@ from tenma import dataload, abilitymodel as am, comparemodel as cm
 from tenma.rulemodel import Rule
 
 YEAR = "2018"
-MONTHDAY = "1028"
-JYOCD = "05"
-RACENUM = "11"
+MONTH = "11"
+DAY = "04"
+
+dic_jyo = {
+    '01': '札幌',
+    '02': '函館',
+    '03': '福島',
+    '04': '新潟',
+    '05': '東京',
+    '06': '中山',
+    '07': '中京',
+    '08': '京都',
+    '09': '阪神',
+    '10': '小倉'
+}
+
+def func(x):
+    if x < 1:
+        return 1
+    else:
+        return int(x)
+
+
+def get_jyo(x):
+    if x in dic_jyo.keys():
+        return dic_jyo[x]
+    else:
+        return ''
 
 if __name__ == "__main__":
     df = dataload.load()
-    df['odds'] = df['odds'].astype(np.int32) / 10.0
-    df['reward'] = ((df['kakuteijyuni'] == "01") * df['odds']) + ((df['kakuteijyuni'] != "01") * -1.0)
     df['predict'] = am.predict(df)
-    df = df.pipe(lambda df: df[df['year'] == "2018"])
+    df['jyoname'] = df['jyocd'].map(get_jyo)
 
-    result = []
-    rule = Rule("rule.csv")
-    for x, y, reward in df[['odds', 'predict', 'reward']].values:
-        action = rule.get_nearly([x, y])
-        if action:
-            result.append(reward)
-        else:
-            result.append(0)
+    df_text = df.pipe(lambda df: df[
+        (df['year'] == '2018')
+    &   (df['monthday'] == MONTH + DAY)
+    ]).sort_values(['jyocd', 'racenum', 'predict']).groupby(
+        ['jyocd', 'racenum']
+    ).tail(1)[['jyoname', 'racenum', 'bamei']]
+    df_text.columns = ['競馬場', 'レース番号', '馬名']
+    text_table = df_text.to_html(index=False)
 
-    print(result)
-    print(sum(result))
+    text = """
+    {}月{}日（）の競馬予想
+    <p>人工知能による競馬予想。{}月{}日の注目馬をピックアップ</p>
+
+    {}
+    """.format(MONTH, DAY, MONTH, DAY, text_table)
+    with open('blog_text.txt', 'w') as f:
+        f.write(text)
