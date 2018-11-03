aaa072d6ec45e15c32eabfc48a95f21bb9a9e3a5
KTaskn <korai.kinjo@gmail.com>
2018-11-03 14:45:12 +0900

    ルールモデルは実装しない

--- tenma/learner.py
+++ tenma/learner.py
@@ -6,28 +6,5 @@ from tenma.rulemodel import Rule
 
 if __name__ == "__main__":
     df = dataload.load()
-    # abilitymodel.learn(df)
-    # comparemodel.learn(df)
-
-    df['odds'] = df['odds'].astype(np.int32) / 10.0
-    df['reward'] = ((df['kakuteijyuni'] == "01") * df['odds']) + ((df['kakuteijyuni'] != "01") * -1.0)
-    df['predict'] = abilitymodel.predict(df)
-
-    rule = Rule()
-    for i in range(50):
-        for x, y, reward in df.sample(2500)[['odds', 'predict', 'reward']].values:
-            action = rule.get_nearly([x, y])
-            if action:
-                rule.set_data(reward, action, [x, y])
-            else:
-                rule.set_data(0, action, [x, y])
-                
-        rule.set_next()
-        rule.init_data()
-
-    pd.DataFrame({
-        "odds": np.array(rule.l_point)[:, 0],
-        "predict": np.array(rule.l_point)[:, 1],
-        "reward": rule.l_reward,
-        "action": rule.l_action,
-    }).to_csv('rule.csv')
+    abilitymodel.learn(df)
+    comparemodel.learn(df)
