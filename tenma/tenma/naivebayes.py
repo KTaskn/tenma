eec00eab639d06d121f8488ca0e8fb5257538834
KTaskn <korai.kinjo@gmail.com>
2019-01-13 12:12:11 +0900

    サマリを外のクラスにする

--- tenma/tenma/naivebayes.py
+++ tenma/tenma/naivebayes.py
@@ -5,9 +5,37 @@ import pandas as pd
 from tenma import dataload
 from datetime import datetime
 
+class NaiveBayesSummary():
+    def __init__(self):
+        self.__probability = np.nan
+        self.__summary = []
+
+    def set_probability(self, p):
+        self.__probability = p
+    
+    def set_entity(self, name, p):
+        self.__summary.append((name, p))
+
+    def get_probability(self):
+        return self.__probability
+
+    def get_entity(self, name=None):
+        if name is None:
+            return self.__summary
+        else:
+            return dict(self.__summary)[name]
+
+    def get_bad_entity(self, num=0):
+        return sorted(self.__summary, key=lambda x: x[1])[num]
+
+    
+    def get_good_entity(self, num=0):
+        return sorted(self.__summary, key=lambda x: x[1], reverse=True)[num]
+
+
 class NaiveBayes():
     def __init__(self, df, P_B, l_name_A, l_jyoken_A, name_B, jyoken_B):
-        self.__init_instance()
+        self.__summary = NaiveBayesSummary()
 
         self.df = df
         self.p_b = P_B
@@ -17,45 +45,17 @@ class NaiveBayes():
         self.jyoken_B = jyoken_B
 
     def predict(self):
-        self.__init_probability()
-        self.__init_summary()
+        self.__summary = NaiveBayesSummary()
         _df = self.df
         _p_b = self.p_b
         _name_B = self.name_B
         _jyoken_B = self.jyoken_B
         for name_A, jyoken_A in zip(self.l_name_A, self.l_jyoken_A):
             _p_b = self.__P_BA(_df, _p_b, name_A, jyoken_A, _name_B, _jyoken_B)
-        self.__probability = _p_b
-
-    def __init_instance(self):
-        self.__init_probability()
-        self.__init_summary()
-
-    def __init_probability(self):
-        self.__probability = np.nan
-
-    def __init_summary(self):
-        self.__summary = []
+        self.__summary.set_probability(_p_b)
     
-    def set_summary(self, name, p):
-        self.__summary.append((name, p))
-
-    def get_probability(self):
-        return self.__probability
-
-    def get_summary(self, name=None):
-        if num is None:
-            return self.__summary
-        else:
-            return dict(self.__summary)[name]
-
-    def get_bad_summary(self, num=0):
-        return sorted(self.__summary, key=lambda x: x[1])[num]
-
-    
-    def get_good_summary(self, num=0):
-        return sorted(self.__summary, key=lambda x: x[1], reverse=True)[num]
-
+    def get_summary(self):
+        return self.__summary
 
     # P(A)
     def __P_A(self, df, col_A, A):
@@ -90,7 +90,7 @@ class NaiveBayes():
         else:
             result_p = np.log10(numerator / mask.astype(int).sum())
 
-        self.set_summary(col_A, result_p)
+        self.__summary.set_entity(col_A, result_p)
         return result_p
 
     # P_BA
@@ -122,17 +122,14 @@ def make_dic_p(df_all, df_grp, l_col, NUM):
                 jyuni
             )
             nb.predict()
-            dic_tmp[row['kettonum']] = nb.get_probability()
-            from pprint import pprint
-            pprint(nb.get_good_summary())
-            pprint(nb.get_bad_summary())
+            dic_tmp[row['kettonum']] = nb.get_summary()
         dic_p[jyuni] = dic_tmp
     return dic_p
 
 def get_p_rentan(tpl, dic):
     p = 0.0
     for idx, id in enumerate(tpl):
-        p += dic["%02d" % (idx + 1)][id]
+        p += dic["%02d" % (idx + 1)][id].get_probability()
     return p
 
 def NaiveBayesModel(df, df_target, l_col, NUM):
